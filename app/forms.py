80e66c7172f6148d2877b2ecb6e5a71c8835fb9e
thapes <stuczynski.thales@gmail.com>
2021-07-04 20:51:27 -0300

    Add files via upload

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+# app/forms.py
+from django import forms
+from .models import *
+from durationwidget.widgets import TimeDurationWidget
+
+class RecipeForm(forms.ModelForm):
+    prep = forms.DurationField(widget=TimeDurationWidget(show_days=False, show_hours=True, show_minutes=True, show_seconds=False), required=False)
+    cook = forms.DurationField(widget=TimeDurationWidget(show_days=False, show_hours=True, show_minutes=True, show_seconds=False), required=False)
+    
+    class Meta:
+        model = Recipe
+        fields = '__all__'
+        exclude = ('author',)
